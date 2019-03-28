41667f089da9879c6c4ed516dadd81766332ed9d
Natsulelou <nutshapon_lelou@hotmail.com>
2019-03-28 09:36:11 +0700

    add role

--- src/modules/product/policy/policy.js
+++ src/modules/product/policy/policy.js
@@ -14,7 +14,7 @@ acl = new acl(new acl.memoryBackend());
 exports.invokeRolesPolicies = function() {
   acl.allow([
     {
-      roles: ["admin", "user"],
+      roles: ["admin", "user", "owner", "staff", "stockstaff", "packstaff"],
       allows: [
         {
           resources: "/api/products",
